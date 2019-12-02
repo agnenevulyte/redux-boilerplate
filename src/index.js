5fbb68168b5416af5bd2036dec4c9bbc03493369
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 08:17:44 +0000

    import Provider and store in the main index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from "./Redux/store";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
