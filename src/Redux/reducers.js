8cee359f11d4272f127753e1f5418e6cacdb46a2
Agne.Nevulyte-at-985443664070 <agne.nevulyte@finsaeurope.com>
2019-12-02 08:16:29 +0000

    combine reducers

--- src/Redux/reducers.js
+++ src/Redux/reducers.js
@@ -1,3 +1,10 @@
-import { combinedReducers } from "redux";
+import { combineReducers } from "redux";
+import appStateReducer from "./appStateReducer";
+import loginReducer from "./loginReducer";
 
-const rootReducer = combinedReducers();
+const rootReducer = combineReducers({
+  appState: appStateReducer,
+  login: loginReducer
+});
+
+export default rootReducer;
